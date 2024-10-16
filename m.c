3279c7b81ba844693b7d9acf11dfae6cbb26a3af
ietsnut <marijnbrussel@hotmail.com>
2024-10-16 23:32:51 +0200

    refactor

--- m.c
+++ m.c
@@ -1,67 +1,103 @@
-#include "rogueutil.h"
-#include <stdlib.h> /* for srand() / rand() */
-#include <stdio.h>
-#include <ctype.h>  /* for isalpha() */
+#include <stdbool.h>
+#include <stdint.h>
 
-int box_x = 1;   // Initial x position of the box
-int box_y = 1;   // Initial y position of the box
-int box_width = 20;
-int box_height = 5;
+#ifdef __AVR__
+    #include <avr/io.h>
+    #include <avr/interrupt.h>
+    #include <avr/sleep.h>
+    #include <util/delay.h>
+    #define wait sleep_mode()
+#else
+    #include <stdio.h>
+    #include <string.h>
+    #include <signal.h> 
+    #define wait pause()
+    #ifdef _WIN32
+        #include <windows.h>
+        #define write(ptr, size) fwrite(ptr, size, 1, stdout)
+        #define read(ptr, size)  fread(ptr, 1, size, stdin)
+    #else
+        #include <unistd.h>
+        #define write(ptr, size) write(STDOUT_FILENO, ptr, size)
+        #define read(ptr, size)  read(STDIN_FILENO, ptr, size)
+    #endif
+#endif
 
-void draw() {
-    cls();  // Clear the screen
-    drawBox(box_x, box_y, box_width, box_height);  // Draw the box at the current coordinates
-    locate(box_x + 1, box_y + 1);
+#define until(cond) while (!(cond))
+#define loop for(;;)
+
+#pragma pack(push, 1)
+typedef struct {
+    uint8_t id      : 8; 
+    uint8_t state   : 8;
+} Automata;
+#pragma pack(pop)
+
+#define COUNT(...) (sizeof((Automata[]){ __VA_ARGS__ }) / sizeof(Automata))
+#define AUTOMATAS(...) Automata automatas[COUNT(__VA_ARGS__)] = { __VA_ARGS__ }
+
+static inline void print(Automata a) {
+    printf("ID: %d, STATE: %d\n",  a.id, a.state); 
+}
+
+static inline void prints(Automata a[]) {
+    for (int i = 0; i < 4; i++) {
+        print(a[i]);
+    }
+}
+
+static inline void out(Automata a) {
+    write(&a, 2);
     fflush(stdout);
 }
 
-int main(void) {
-    hidecursor();             // Hide cursor
-    saveDefaultColor();        // Save default color settings
-    draw();
-    int quit = 0;
-    while (!quit) {
-        if (kbhit()) {
-            int key = getkey();
+static inline void in(Automata* a) {
+    until(read(a, sizeof(*a)) == 2);
+}
 
-            switch (key) {
-                case KEY_ESCAPE:  // Exit the loop when Escape is pressed
-                    quit = 1;
-                    break;
-                case KEY_LEFT:  // Move left
-                    if (box_x > 0) {
-                        box_x--;
-                        draw();
-                    }
-                    break;
-                case KEY_RIGHT:  // Move right
-                    if (box_x < tcols() - 3) {
-                        box_x++;
-                        draw();
-                    }
-                    break;
-                case KEY_UP:  // Move up
-                    if (box_y > 0) {
-                        box_y--;
-                        draw();
-                    }
-                    break;
-                case KEY_DOWN:  // Move down
-                    if (box_y < trows() - box_height) {
-                        box_y++;
-                        draw();
-                    }
-                    break;
-                default:
-                    break;
-            }
-        }
+volatile bool running = true;
+
+// SIGUSR1, SIGUSR2
+
+void interrupt(int sig) { 
+    printf("Caught signal %d\n", sig); 
+    running = false;
+} 
+
+int main(int argc, char *argv[]) {
+
+    signal(SIGINT, interrupt); 
+
+    loop {
+
+        wait;
+        raise(SIGINT);
+        printf("Breaking\n");
+        break;
+
     }
 
-    cls();           // Clear the screen when exiting
-    resetColor();    // Reset color settings
-    showcursor();    // Show the cursor again
-    fflush(stdout);  // Ensure everything is printed
-
-    return 0;
 }
+
+    /*
+
+    printf("You have entered %d arguments:\n", argc);
+ 
+    for (int i = 0; i < argc; i++) {
+        printf("%s\n", argv[i]);
+    }
+
+    if (argc >= 2 && strcmp(argv[1], "debug") == 0) {
+          printf("DEBUGGING MODE ON\n");
+    }
+
+    AUTOMATAS(
+        { 0, 0 },
+        { 1, 0 },
+        { 2, 2 },
+        { 3, 1 }
+    );
+
+    printf("size == %zu \n" , sizeof(Automata) * 8);
+
+    prints(automatas);*/
